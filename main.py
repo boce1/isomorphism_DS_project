86f59aa7bf24c5f21abb2a90cd16c300a11a44c2
Boyan Zarev <zarev.bojan2@gmail.com>
2025-04-06 20:12:56 +0300

    add screenshots

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import color_refinement
 from test_graphs import *
 
-draw_2_graphs(*test8)
+# draw_2_graphs(*test1)
+# print("Test 1, 1-WL test: ", color_refinement.wl_test(*test1, visualize=True))
+# 
+# draw_2_graphs(*test3)
+# print("Test 3, 1-WL test: ", color_refinement.wl_test(*test3, visualize=True))
 
-print(color_refinement.wl_test(*test8, visualize=True, node_size=150))
+draw_2_graphs(*test4, node_size=150)
+print("Test 4, 1-WL test: ", color_refinement.wl_test(*test4, visualize=True, node_size=150))
