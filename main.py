24b59ffa70346612a3e8f8cb0f69343d66c2c615
LuckyOx04 <ivailon27@gmail.com>
2025-04-10 15:50:26 +0300

    Functions are written. Now I need to debug them.

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ from test_graphs import test1, test2, test3, test4, test5, test6, test7, test8
 
 # test algs here
 # TO DO: when all algs are finished, write all test for all algs
+
+tests = [test1, test2, test3, test4, test5, test6, test7, test8]
+for i in range(1, len(tests) + 1):
+    vf_2.G1 = (set(tests[i - 1][0].nodes), set(tests[i - 1][0].edges))
+    vf_2.G2 = (set(tests[i - 1][1].nodes), set(tests[i - 1][1].edges))
+    result = vf_2.vf2_isomorphism(set(), 300)
+    print(result)
+    # print(f"Graph from test{i} is {"ISOMORPHIC" if result[0] else "NOT ISOMORPHIC"}")
+    # print(f"\nMapping is: {[x for x in result[1]]}")
--- vf_2/__init__.py
+++ vf_2/__init__.py
@@ -16,30 +16,49 @@ import itertools
 G1 = (set(),set())
 G2 = (set(),set())
 
-def vf2_isomorphism(mapping):
-    if G1.get_nodes() == set([x[0] for x in mapping]):
+def vf2_isomorphism(mapping, iterations):
+    if iterations <= 0:
+        return (False, mapping)
+    if G1[0] == set([x[0] for x in mapping]):
         return (True, mapping)
     else:
-        candidate_pairs = compute_Pm()
+        candidate_pairs = compute_Pm(mapping)
         for pair in candidate_pairs:
             extended_mapping = extend_mapping(mapping, pair)
-            if cons(extended_mapping) and not cut(extended_mapping):
-                vf2_isomorphism(G1, G2, extended_mapping)
+            if cons(mapping, pair) and not cut(mapping, pair):
+                vf2_isomorphism(extended_mapping, iterations - 1)
 
 def extend_mapping(m, p):
     if p[0] in (G1[0] - set([x[0] for x in m])) and p[1] in (G2[0] - set([x[1] for x in m])):
-        return m.append(p)
+        return m.add(p)
     else:
         return m
 
 def compute_Pm(m):
     G1_nodes = set([x for x in G1[0] - set([y[0] for y in m])])
     G2_nodes = set([x for x in G2[0] - set([y[1] for y in m])])
-    T1 = set([x for x in G1_nodes if (x, y[0]) in G1[1] for y in m])
-    T2 = set([x for x in G2_nodes if (x, y[1]) in G2[1] for y in m])
+    T1 = set([x for x in G1_nodes if ((x, y[0]) in G1[1] for y in m)])
+    T2 = set([x for x in G2_nodes if ((x, y[1]) in G2[1] for y in m)])
 
     if not T1 and not T2:
         return itertools.product(T1, T2)
     else:
         return itertools.product(G1_nodes, G2_nodes)
-    
+
+def cons(m, p):
+    gamma_1 = set([x for x in G1[0] if (p[0], x) in G1[1]])
+    gamma_2 = set([x for x in G2[0] if (p[1], x) in G2[1]])
+    return set([x for x in gamma_2.intersection(set([y[1] for y in m])) if (p[0], [z[0] for z in m if z[1] == x][0]) in G1[1]]) \
+    and set([x for x in gamma_1.intersection(set([y[0] for y in m])) if (p[1], [z[1] for z in m if z[0] == x][0]) in G2[1]])
+
+def cut(m, p):
+    G1_nodes = set([x for x in G1[0] - set([y[0] for y in m])])
+    G2_nodes = set([x for x in G2[0] - set([y[1] for y in m])])
+    T1 = set([x for x in G1_nodes if ((x, y[0]) in G1[1] for y in m)])
+    T2 = set([x for x in G2_nodes if ((x, y[1]) in G2[1] for y in m)])
+    T1_tilde = (G1[0] - set([x[0] for x in m])) - T1
+    T2_tilde = (G2[0] - set([x[1] for x in m])) - T2
+    gamma_1 = set([x for x in G1[0] if (p[0], x) in G1[1]])
+    gamma_2 = set([x for x in G2[0] if (p[1], x) in G2[1]])
+    return len(gamma_2.intersection(T2)) < len(gamma_1.intersection(T1)) \
+    and len(gamma_2.intersection(T2_tilde)) < len(gamma_1.intersection(T1_tilde))
