f62dc4f9ec02e7361669ac4b4a0f82253dda652b
Boyan Zarev <zarev.bojan2@gmail.com>
2025-04-17 20:40:53 +0300

    edit main and edit reamde

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import color_refinement
 import vf_2
-import networkx as nx
 from test_graphs import test1, test2, test3, test4, test5, test6, test7, test8, draw_2_graphs, draw_graph
 
-# test algs here
-# TO DO: when all algs are finished, write all test for all algs
+def format_result(test_name, is_isomorphic):
+    if is_isomorphic:
+        print(f"{test_name} : Graphs are isomorphic")
+    else:
+        print(f"{test_name} : Graphs are NOT isomorphic")
 
 tests = [test1, test2, test3, test4, test5, test6, test7, test8]
 for i in range(len(tests)):
     test = tests[i]
-    result = vf_2.vf2_isomorphism(test[0], test[1])
+    result_vf_2 = vf_2.vf2_isomorphism(*test)
+    result_color_refinement = color_refinement.wl_test(*test) # visualize=False
+    # add result for third alg
+
     draw_2_graphs(test[0], test[1])
-    print(f"Graph from test{i + 1} is {"ISOMORPHIC" if result else "NOT ISOMORPHIC"}")
+
+    print(f"Test {i + 1}:")
+    format_result("VF2", result_vf_2)
+    format_result("Color Refinement", result_color_refinement)
+    
+    print("---------------------\n")
